38c218439e3df30d60aa72f97e660faf21886c76
harshprasad <hprasad13579@gmail.com>
2024-07-25 20:05:07 +0530

    Login-SignUp Pages

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,85 +1,82 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Validator;
 use App\Models\users;
 use Illuminate\Http\Request;
+use DB;
+use Session;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Hash;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function UserSignUpView()
     {
-        //
+        return view('Logins/SignUp');
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function UserSignUpSuccess()
     {
-        //
+        return view('Logins/SignUpSuccess');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function UserLoginView()
     {
-        //
+        return view('Logins/Login');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\users  $users
-     * @return \Illuminate\Http\Response
-     */
-    public function show(users $users)
+    public function SignUp(Request $request)
     {
-        //
+        $rules = [
+            'email' => 'required|email|unique:users',
+            'firstname' => 'required|string|max:255|regex:/^[a-zA-Z ]+$/',
+            'lastname' => 'required|string|max:255|regex:/^[a-zA-Z ]+$/',
+            'password' => 'required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) 
+        {
+            return redirect('SignUpView')->withErrors($validator)->withInput();
+        }
+        $firstname= $request->input('firstname');
+        $lastname= $request->input('lastname');
+        $email= $request->input('email');
+        $password= $request->input('password');
+        $hashedpassword=Hash::make($password);
+        $email_subs=$request->input('email_subs');
+        if($email_subs==NULL)
+            DB::insert('insert into users (firstname,lastname,email,password,email_subs) values(?,?,?,?,?)',[$firstname,$lastname,$email,$hashedpassword,0] );
+        else
+        DB::insert('insert into users (firstname,lastname,email,password,email_subs) values(?,?,?,?,?)',[$firstname,$lastname,$email,$hashedpassword,$email_subs] );
+        return redirect()->route('SignUpSuccess');   
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\users  $users
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(users $users)
+    public function Login(Request $request)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\users  $users
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, users $users)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\users  $users
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(users $users)
-    {
-        //
+        $rules = [
+            'email'=>'required|string|email|exists:users',
+            'password' => 'required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) 
+        {
+            return redirect('UserLoginView')->withInput()->withErrors($validator);
+        }
+        else
+        {
+            $email = $request->input('email');
+            $req_email = DB::table('users')->where('email', $email)->first();
+            if(DB::table('users')->where('email',$user1)->exists())
+            {
+                $password= $request->input('password');
+                $user = DB::table('users')->where('email', $email)->value('password');
+                if(HASH::check($password,$user))
+                {
+                    Session::put('user',$user1);
+                    return redirect()->route('StudentDashboard');
+                }
+                    
+                else
+                    return back()->withInput()->withErrors(['password' => 'Wrong Password!']);
+            }
+        }
     }
 }
--- database/migrations/2024_07_25_060244_create_users_table.php
+++ database/migrations/2024_07_25_060244_create_users_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('email_subs')->default(false);
             $table->timestamps();
         });
     }
